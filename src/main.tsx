e75720218da21cd4c2ccc07f42ae18f44bab2393
GuyTalmi <guytalmi@gmail.com>
2023-04-26 23:27:11 +0300

    Add react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App';
 import './index.css';
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000,   // 5 minutes
+//       staleTime: 10 * 1000, // 10 seconds
+//       refetchOnWindowFocus: true,
+//       refetchOnReconnect: true,
+//       refetchOnMount: true,
+//     }
+//   }
+// });
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
