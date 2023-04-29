bd1e4fd230e88300c98fecd55dc781f8ae5a91cf
GuyTalmi <guytalmi@gmail.com>
2023-04-29 15:21:29 +0300

    Add TasksContext.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,31 @@ import './App.css';
 import TodoForm from './react-query/TodoForm';
 import TodoList from './react-query/TodoList';
 import Counter from './state-management/Counter';
+import LoginStatus from './state-management/authStatus';
 import TaskList from './state-management/TaskList';
+import { useReducer } from 'react';
+import tasksReducer from './state-management/reducers/tasksReducer';
+
+import { Dispatch } from 'react';
+import TasksContext from './state-management/context/tasksContext';
+import NavBar from './state-management/NavBar';
 
 function App() {
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
   return (
     <>
       {/* <TodoForm />
       <TodoList /> */}
-
       {/* <Counter /> */}
+      {/* <TaskList /> */}
+      {/* <LoginStatus /> */}
 
-      <TaskList />
+
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <TaskList />
+      </TasksContext.Provider>
     </>
   );
 }
---
+++ src/state-management/authStatus.tsx
@@ -0,0 +1,29 @@
+import { useReducer } from "react";
+import authReducer from "./reducers/loginStatusReducer";
+
+const LoginStatus = () => {
+  const [user, dispatch] = useReducer(authReducer, '');
+
+  if (user)
+    return (
+      <>
+        <div>
+          <span className="mx-2">{user}</span>
+          <a onClick={() => dispatch({ type: 'LOGOUT' })}
+            href="#">
+            Logout
+          </a>
+        </div>
+      </>
+    );
+  return (
+    <div>
+      <a onClick={() => dispatch({ type: 'LOGIN', userName: 'mosh.hamedani' })}
+        href="#">
+        Login
+      </a>
+    </div>
+  );
+};
+
+export default LoginStatus;
